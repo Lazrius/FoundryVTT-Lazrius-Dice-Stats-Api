c132fc767d8e6b0c2940f647f496e412a8baa31c
Lazrius <lazdisco@gmail.com>
2022-07-11 16:43:21 +0100

    add isDm to user request

--- Source/Models/Requests/NewUserRequest.ts
+++ Source/Models/Requests/NewUserRequest.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsBoolean, IsNotEmpty, IsString } from "class-validator";
 
 export default class NewUserRequest {
 	@IsString()
@@ -8,4 +8,7 @@ export default class NewUserRequest {
 	@IsString()
 	@IsNotEmpty()
 	userId: string;
+
+	@IsBoolean()
+	isDm: boolean;
 }
