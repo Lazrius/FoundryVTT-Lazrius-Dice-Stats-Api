0c9ecb233988b7d55de9e364c39fec52dd0a2dc9
Lazrius <lazdisco@gmail.com>
2022-08-11 14:23:01 +0100

    fix CLI not passing in args correctly

--- cli.js
+++ cli.js
@@ -9,10 +9,10 @@ try {
 			execSync('node dist/Server.js ' + process.argv.slice(3).join(' '), { stdio: 'inherit', cwd: __dirname });
 			break;
 		case "update":
-			execSync('npm run migration:up ' + process.argv.slice(3).join(' '), { stdio: 'inherit', cwd: __dirname });
+			execSync('npm run migration:up -- ' + process.argv.slice(3).join(' '), { stdio: 'inherit', cwd: __dirname });
 			break;
 		case "revert":
-			execSync('npm run migration:down' + process.argv.slice(3).join(' '), { stdio: 'inherit', cwd: __dirname });
+			execSync('npm run migration:down -- ' + process.argv.slice(3).join(' '), { stdio: 'inherit', cwd: __dirname });
 			break;
 		default:
 			console.error('No valid command was specified. Valid commands are: run, update, revert');
